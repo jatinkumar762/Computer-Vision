47201449b5238043bc4f8d3344ecb15d8e49e363
Jatin Kumar <jatin.kumar762@gmail.com>
2020-01-27 20:00:46 +0530

    Create Canny_Edge_Detector.py

---
+++ Assignment-1/Canny_Edge_Detector.py
@@ -0,0 +1,139 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage
+
+def sobel_filters(img):
+    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
+    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
+    
+    #Ix = ndimage.filters.convolve(img, Kx)
+    #Iy = ndimage.filters.convolve(img, Ky)
+    
+    Ix = cv2.filter2D(img,-1,Kx)
+    Iy = cv2.filter2D(img,-1,Ky)
+
+    G = np.hypot(Ix, Iy)
+    G = G / G.max() * 255
+    theta = np.arctan2(Iy, Ix)
+    
+    return (G, theta) 
+
+
+def non_max_suppression(img, D):
+    M, N = img.shape
+    Z = np.zeros((M,N), dtype=np.int32)
+    angle = D * 180. / np.pi
+    angle[angle < 0] += 180
+
+    
+    for i in range(1,M-1):
+        for j in range(1,N-1):
+            try:
+                q = 255
+                r = 255
+                
+               #angle 0
+                if (0 <= angle[i,j] < 22.5) or (157.5 <= angle[i,j] <= 180):
+                    q = img[i, j+1]
+                    r = img[i, j-1]
+                #angle 45
+                elif (22.5 <= angle[i,j] < 67.5):
+                    q = img[i+1, j-1]
+                    r = img[i-1, j+1]
+                #angle 90
+                elif (67.5 <= angle[i,j] < 112.5):
+                    q = img[i+1, j]
+                    r = img[i-1, j]
+                #angle 135
+                elif (112.5 <= angle[i,j] < 157.5):
+                    q = img[i-1, j-1]
+                    r = img[i+1, j+1]
+
+                if (img[i,j] >= q) and (img[i,j] >= r):
+                    Z[i,j] = img[i,j]
+                else:
+                    Z[i,j] = 0
+
+            except IndexError as e:
+                pass
+    
+    return Z
+
+def threshold(img, lowThresholdRatio=0.05, highThresholdRatio=0.09):
+    
+    highThreshold = img.max() * highThresholdRatio;
+    lowThreshold = highThreshold * lowThresholdRatio;
+    
+    M, N = img.shape
+    res = np.zeros((M,N), dtype=np.int32)
+    
+    weak = np.int32(100)
+    strong = np.int32(200)
+    
+    strong_i, strong_j = np.where(img >= highThreshold)
+    zeros_i, zeros_j = np.where(img < lowThreshold)
+    
+    weak_i, weak_j = np.where((img <= highThreshold) & (img >= lowThreshold))
+    
+    res[strong_i, strong_j] = strong
+    res[weak_i, weak_j] = weak
+    
+    return (res, weak, strong)
+
+def hysteresis(img, weak, strong=200):
+    M, N = img.shape  
+    for i in range(1, M-1):
+        for j in range(1, N-1):
+            if (img[i,j] == weak):
+                try:
+                    if ((img[i+1, j-1] == strong) or (img[i+1, j] == strong) or (img[i+1, j+1] == strong)
+                        or (img[i, j-1] == strong) or (img[i, j+1] == strong)
+                        or (img[i-1, j-1] == strong) or (img[i-1, j] == strong) or (img[i-1, j+1] == strong)):
+                        img[i, j] = strong
+                    else:
+                        img[i, j] = 0
+                except IndexError as e:
+                    pass
+    return img
+
+
+
+img=cv2.imread('flower.jpg',0)
+cv2.imshow('Flower', img) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
+
+
+blur=cv2.GaussianBlur(img,(5,5),0)
+cv2.imshow('Blurred Image', blur) 
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
+
+G,theta = sobel_filters(img)
+plt.imshow(np.rint(G).astype(int),cmap = 'gray')
+plt.show()
+
+Z = non_max_suppression(G,theta)
+plt.imshow(Z,cmap = 'gray')
+plt.show()
+
+
+res, weak, strong = threshold(Z)
+plt.imshow(res,cmap = 'gray')
+plt.show()
+
+
+eimg = hysteresis(res, weak)
+plt.imshow(eimg,cmap = 'gray')
+plt.show()
+
+
+
+edges = cv2.Canny(np.uint8(img),100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
